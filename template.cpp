532f36ca23e29c2b3946072285e649039cba26b8
Raul Guzman <rguzmanc.161@gmail.com>
2016-05-07 20:26:46 -0500

    add problems

--- template.cpp
+++ template.cpp
@@ -1,19 +1,26 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <map>
-#include <stack>
-#include <queue>
-
-#define ll long long;
-#define debug cout<<"ok"<<endl;
-#define loop(i,n) for(int i = 0; i < n ; i++)
-#define mem(array,value) memset(array,value,sizeof(array))
-#define pb(x) push_back(x)
-#define read(n) scanf("%d",&n)
-
+#include <bits/stdc++.h>
 using namespace std;
+
+#define debug(x) cout<<#x<<" : "<<x<<"\n"
+#define debug2(x, y) cout << #x << " : " << x << " - " << #y << " : " << y << "\n"
+#define debug3(i,n,array) for(int i = 0; i < n ; i++) cout<<#array[i]<<" : "<<array[i]<<"\n"
+#define test() cout<<"Hello world"<<"\n"
+
+#define loop(i,n) for(int i = 0; i < n ; i++)
+#define read(n) cin >> n
+#define readSS(ss1,ss2) cin >> ss1 >> ss2
+#define write(n) cout << n << "\n"
+
+#define sz(array) (int)array.size()
+#define mset(array,value) memset(array,value,sizeof(array))
+#define vi vector<int>
+#define vll vector<ll>
+#define msi map<int,string>
+#define mss map<string,string> 
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
 
 int main(){
 
