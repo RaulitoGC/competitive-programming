312e6129f7346525aa00cba2b1be09bf79d70510
Raul Guzman <rguzmanc.161@gmail.com>
2020-05-01 17:05:48 -0500

    Feat: Add codeforce problems

---
+++ CodeForces/A. Ichihime and Triangle.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cout<<#x<<" : "<<x<<"\n"
+#define debug2(x, y) cout << #x << " : " << x << " - " << #y << " : " << y << "\n"
+#define debug3(i,n,array) for(int i = 0; i < n ; i++) cout<<#array[i]<<" : "<<array[i]<<"\n"
+#define test() cout<<"Hello world"<<"\n"
+#define z() usleep(2000)
+
+#define loop(i,n) for(int i = 0; i < n ; i++)
+
+#define sz(array) (int)array.size()
+#define mset(array,value) memset(array,value,sizeof(array))
+
+#define vi vector<int>
+#define vll vector<ll>
+#define msi map<int,string>
+#define mss map<string,string> 
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+
+
+int main(){
+    int t;
+    long long a, b, c, d;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<b<<" "<<c<<endl;
+    }
+
+	return 0;
+}
