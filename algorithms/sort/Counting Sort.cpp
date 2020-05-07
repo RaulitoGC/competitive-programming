fdff308b5238d718db5fff6596a93d75208b0138
Raul Guzman <rguzmanc.161@gmail.com>
2020-05-06 23:11:39 -0500

    Feat: add Couting sort algorithm and update .gitignore

---
+++ algorithms/sort/Counting Sort.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+
+#define debug(x) cout<<#x<<" : "<<x<<"\n"
+#define debug2(x, y) cout << #x << " : " << x << " - " << #y << " : " << y << "\n"
+#define debug3(array,n) for(int i = 0; i < n ; i++) cout<<i<<" : "<<array[i]<<"\n"
+#define separator() cout << "------------------------" << endl;
+#define test() cout<<"Hello world"<<"\n"
+#define z() Sleep(2000)
+
+#define loop(i,n) for(int i = 0; i < n ; i++)
+
+#define sz(array) (int)array.size()
+#define mset(array,value) memset(array,value,sizeof(array))
+
+#define vi vector<int>
+#define vll vector<ll>
+#define msi map<int,string>
+#define mss map<string,string> 
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+void countingSort(int* arr, int* out, int n){
+
+    int L = INT_MAX,R = INT_MIN;
+    for(int i = 0 ; i < n ; i++){
+        if( arr[i] < L) L = arr[i];
+        if( arr[i] > R) R = arr[i];
+    }
+    
+    int k = R-L+1;
+    int F[k];
+    
+    for(int i = 0 ; i < k ; i++) F[i] = 0;
+    
+    for(int i = 0 ; i < n ; i++) F[arr[i] - L]++;
+    
+    for(int i = 1 ; i < k ; i++) F[i] += F[i-1];
+    
+    for(int i = 0 ; i < n ; i++){
+        out[F[arr[i] - L]] = arr[i]; 
+        F[arr[i] - L]--;
+    }
+    
+}
+
+void librarySort(int* arr, int n){
+    sort(arr,arr+n);
+}
+
+int main(){
+
+    int arr[] = { 10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,
+                    10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1,10,9,8,7,6,5,4,3,2,1};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int out[n];
+    debug(n);
+	clock_t t = clock();
+
+    librarySort(arr, n);
+    //countingSort(arr,out,n);
+
+    t = clock() - t;
+    //debug3(out,n);
+    debug(t);
+    return 0;
+}
