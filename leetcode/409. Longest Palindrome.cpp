7eea9514852629574a841a1adcade2aeb29b18c1
Raul Guzman <rguzmanc.161@gmail.com>
2020-08-14 19:31:03 -0500

    add leetcode daily problem

---
+++ leetcode/409. Longest Palindrome.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define test() cerr<<"Hello Family"<<"\n"
+#define sz(array) (int)array.size() 
+#define mset(array,value) memset(array,value,sizeof(array)) // [0] or [-1]
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define popb pop_back
+#define all(v) v.begin(),v.end()
+
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int,int>
+#define pii pair<int, int>
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "T" : "F");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "<" + to_string(p.first) + ", " + to_string(p.second) + ">";
+}
+
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 0
+#endif
+
+/****************************************************************/
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> um;
+        int sz = s.size();
+        for(int i = 0 ; i < sz ; i++){
+            um[s[i]]++;
+        }
+        
+        int res = 0;
+        bool flag = false;
+        unordered_map<char,int>::iterator it;
+        for(it = um.begin() ; it != um.end() ; it++){
+            if(it->second % 2 != 0) flag = true;
+            res += ((it->second/2) * 2);
+        }
+        if(flag) res++;
+        return res;
+        
+    }
+};
+
+int32_t main(){
+
+  #ifdef _LOCAL_INPUT
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+  #endif
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cin.tie(0);
+
+  string ss;
+  Solution solution;
+  while(cin >> ss){
+    int res = solution.longestPalindrome(ss);
+    cout << res << endl;
+  }
+  
+
+	return 0;
+}
