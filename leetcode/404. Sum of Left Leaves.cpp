0961eba70ca0b880e5df73456593a7274573450b
Raul Guzman <rguzmanc.161@gmail.com>
2020-08-24 10:36:08 -0500

    feature: add leetcode problem

---
+++ leetcode/404. Sum of Left Leaves.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define test() cerr<<"Hello Family"<<"\n"
+#define sz(array) (int)array.size() 
+#define mset(array,value) memset(array,value,sizeof(array)) // [0] or [-1]
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define popb pop_back
+#define all(v) v.begin(),v.end()
+
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int,int>
+#define pii pair<int, int>
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+ 
+/*****************************************************************/
+
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "T" : "F");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "<" + to_string(p.first) + ", " + to_string(p.second) + ">";
+}
+
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 0
+#endif
+
+/****************************************************************/
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    
+  int sum;
+    
+  void run(TreeNode* root, bool isLeft){
+      if( root == NULL) return;
+      if(root->left == NULL && root->right == NULL && isLeft){
+          sum += root->val;
+      }
+      
+      run(root->left, true);
+      run(root->right, false);
+  }
+    
+  int sumOfLeftLeaves(TreeNode* root) {
+      sum = 0;
+      run(root, false);
+      return sum;
+  }
+};
