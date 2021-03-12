e3d19b8ba8689c32a8da18e13919de8dd57f33f6
Raul Guzman <rguzmanc.161@gmail.com>
2021-03-11 23:47:29 -0500

    leetcode: Update  solution for Coin Change problem

--- online-training-platform/leetcode/322. Coin Change.cpp
+++ online-training-platform/leetcode/322. Coin Change.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int dp[amount + 1];
-	    dp[0] = 0;
-        
-        for( int i = 1 ; i <= amount; i++){
-            dp[i] = -1;
-        }
-        
-	    sort(coins.begin(), coins.end());
-
-        for(int j = coins[0] ; j <= amount ; j++){
-            if(dp[j - coins[0]] != -1){
-                dp[j] = dp[j-coins[0]] + 1;
+        int maxNumber = 10000 + amount;
+        vector<int> v(amount + 1, maxNumber);
+        v[0] = 0;
+        int n = coins.size(), coin;
+        for(int i = 0 ; i < n ; i++){
+            coin = coins[i];
+            for(int j = coin ; j <= amount ; j++){
+                v[j] = min(v[j], 1 + v[j - coin]);
             }
         }
-	
-        for( int i = 0 ; i < coins.size(); i++){
-            // cout << coins[i] << endl;
-            for(int j = coins[i] ; j <= amount ; j++){
-                if(dp[j - coins[i]] != -1){
-                    if(dp[j] != -1){
-                        dp[j] = min(dp[j-coins[i]] + 1, dp[j]);
-                    }else{
-                        dp[j] = dp[j-coins[i]] + 1;
-                    }
-
-                }
-            }
-
-        }
-	
-	return dp[amount];
+        
+        return (v[amount] == maxNumber)? -1: v[amount];
     }
 };
